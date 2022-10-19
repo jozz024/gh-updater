c44f94892f2514aec97da14fa92fe574ca1627de
jozz024 <74272560+jozz024@users.noreply.github.com>
2022-10-19 17:40:28 -0500

    switch from minreq to smashnet

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use serde_json::Value;
+use smashnet::types::*;
 
 pub struct ReleaseFinderConfig {
     auth_token: Option<String>,
@@ -44,20 +45,13 @@ impl ReleaseFinderConfig {
         self
     }
 
-    pub fn find_release(self) -> Result<(Option<ReleaseManager>, Option<ReleaseManager>), minreq::Error> {
+    pub fn find_release(self) -> Result<(Option<ReleaseManager>, Option<ReleaseManager>), String> {
         let url = format!("https://api.github.com/repos/{}/{}/releases", self.author, self.repository);
-        let request = minreq::Request::new(minreq::Method::Get, url)
-            .with_header("Accept", "application/vnd.github.v3+json")
-            .with_header("User-Agent", self.client.as_str());
-        let request = if let Some(token) = self.auth_token.as_ref() {
-            request.with_header("Authorization", format!("token {}", token).as_str())
-        } else {
-            request
-        };
-        let response = request.send()?;
-        let json_response: Vec<Value> = match serde_json::from_str(response.as_str()?) {
+        let mut request: Curler = Curler::new();
+        let response = request.get(url).unwrap();
+        let json_response: Vec<Value> = match serde_json::from_str(response.as_str()) {
             Ok(response) => response,
-            Err(_) => return Err(minreq::Error::Other("Failed to parse GitHub JSON Respone!"))
+            Err(_) => return Err("Failed to parse GitHub JSON Respone!".to_string())
         };
 
         let mut latest_release = None;
@@ -66,18 +60,11 @@ impl ReleaseFinderConfig {
         for release in json_response.into_iter() {
             if self.allow_prerelease && latest_prerelease.is_none() && release["prerelease"].as_bool().expect("GitHub Release JSON Invalid!") {
                 let asset_url = release["assets_url"].as_str().expect("GitHub release assets_url is invalid!");
-                let request = minreq::Request::new(minreq::Method::Get, asset_url)
-                    .with_header("Accept", "application/vnd.github.v3+json")
-                    .with_header("User-Agent", self.client.as_str());
-                let request = if let Some(token) = self.auth_token.as_ref() {
-                    request.with_header("Authorization", format!("token {}", token).as_str())
-                } else {
-                    request
-                };
-                let response = request.send()?;
-                let json_response: Vec<Value> = match serde_json::from_str(response.as_str()?) {
+                let mut request: Curler = Curler::new();
+                let response = request.get(asset_url.to_string()).unwrap();
+                let json_response: Vec<Value> = match serde_json::from_str(response.as_str()) {
                     Ok(response) => response,
-                    Err(_) => return Err(minreq::Error::Other("Failed to parse GitHub assets JSON response!"))
+                    Err(_) => return Err("Failed to parse GitHub assets JSON response!".to_string())
                 };
                 latest_prerelease = Some(ReleaseManager {
                     client_name: self.client.clone(),
@@ -89,18 +76,11 @@ impl ReleaseFinderConfig {
             
             if latest_release.is_none() && !release["prerelease"].as_bool().expect("GitHub Release JSON Invalid!") {
                 let asset_url = release["assets_url"].as_str().expect("GitHub release assets_url is invalid!");
-                let request = minreq::Request::new(minreq::Method::Get, asset_url)
-                    .with_header("Accept", "application/vnd.github.v3+json")
-                    .with_header("User-Agent", self.client.as_str());
-                let request = if let Some(token) = self.auth_token.as_ref() {
-                    request.with_header("Authorization", format!("token {}", token).as_str())
-                } else {
-                    request
-                };
-                let response = request.send()?;
-                let json_response: Vec<Value> = match serde_json::from_str(response.as_str()?) {
+                let mut request: Curler = Curler::new();
+                let response = request.get(asset_url.to_string()).unwrap();
+                let json_response: Vec<Value> = match serde_json::from_str(response.as_str()) {
                     Ok(response) => response,
-                    Err(_) => return Err(minreq::Error::Other("Failed to parse GitHub assets JSON response!"))
+                    Err(_) => return Err("Failed to parse GitHub assets JSON response!".to_string())
                 };
                 latest_release = Some(ReleaseManager {
                     client_name: self.client.clone(),
@@ -139,61 +119,19 @@ impl ReleaseManager {
         for asset in self.assets.iter() {
             if let Some(asset_name) = asset["name"].as_str() {
                 if asset_name == name.as_ref() {
-                    let request = minreq::Request::new(minreq::Method::Get, asset["url"].as_str().expect("GitHub Asset JSON Invalid!"))
-                        .with_header("Accept", "application/octet-stream")
-                        .with_header("User-Agent", self.client_name.as_str());
-                    let request = if let Some(token) = self.auth_token.as_ref() {
-                        request.with_header("Authorization", format!("token {}", token).as_str())
-                    } else {
-                        request
-                    };
-                    let response = match request.send() {
-                        Ok(response) => response.into_bytes(),
+                    let mut request: Curler = Curler::new();
+                    let mut buffer = Vec::new();
+                    let response = match request.get_bytes(asset["url"].to_string(), &mut buffer){
+                        Ok(response) => {
+                            dbg!(response.clone());
+                            &buffer
+                        },
                         Err(e) => {
                             println!("{:?}", e);
                             return None;
                         }
                     };
-                    return Some(response)
-                }
-            }
-        }
-        None
-    }
-
-    pub fn get_asset_by_name_with_progress<S: AsRef<str>>(&self, name: S, on_download: impl Fn(usize, usize)) -> Option<Vec<u8>> {
-        for asset in self.assets.iter() {
-            if let Some(asset_name) = asset["name"].as_str() {
-                if asset_name == name.as_ref() {
-                    let request = minreq::Request::new(minreq::Method::Get, asset["url"].as_str().expect("GitHub Asset JSON Invalid!"))
-                        .with_header("Accept", "application/octet-stream")
-                        .with_header("User-Agent", self.client_name.as_str());
-                    let request = if let Some(token) = self.auth_token.as_ref() {
-                        request.with_header("Authorization", format!("token {}", token).as_str())
-                    } else {
-                        request
-                    };
-                    let response = match request.send_lazy() {
-                        Ok(response) => response,
-                        Err(e) => {
-                            println!("{:?}", e);
-                            return None;
-                        }
-                    };
-                    let mut vec = Vec::new();
-                    for (count, result) in response.into_iter().enumerate() {
-                        let (byte, length) = match result {
-                            Ok(res) => res,
-                            Err(e) => {
-                                println!("{:?}", e);
-                                return None;
-                            }
-                        };
-                        on_download(count, length);
-                        vec.reserve(length);
-                        vec.push(byte);
-                    }
-                    return Some(vec)
+                    return Some(buffer)
                 }
             }
         }
